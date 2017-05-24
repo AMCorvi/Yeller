570423dd9b627c9e1e726c427115de337fec05a0
AMCorvi <amcorvi@icloud.com>
2017-05-24 00:20:47 -0400

    - Added fade to style - Animated note for blank entry

--- js/yell.js
+++ js/yell.js
@@ -1,21 +1,22 @@
 $(function() {
 
 
+
     var yell = function(event){
         var yellVal = $('.yellbox').val();
 
         if( yellVal !== "" && (event.which === 13 || event.type === 'click')){
            
+          $(advice).css({display: "none"});
           // add input content to list on click
           $('ul').prepend( "<li>" + yellVal + "</li>" )
           //clear input field
           return $('.yellbox').val("");
 
 
+        } else if (yellVal === "" && (event.which === 13 || event.type === 'click') )
+            return $(advice).css({display: 'block'})
         }
-
-
-    }
 
     $('.yellbutton').on('click', yell )
     $(".yellbox").on('keydown', yell)
