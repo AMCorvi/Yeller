e9606e568e4dff9fd382c95c881407c2d141e3f0
AMCorvi <amcorvi@icloud.com>
2017-05-24 02:58:09 -0400

    - Created and integrated delete feature

--- js/yell.js
+++ js/yell.js
@@ -9,17 +9,23 @@ $(function() {
            
           $(advice).css({display: "none"});
           // add input content to list on click
-          $('ul').prepend( "<li>" + yellVal + "</li>" )
+          $('ul').prepend( "<li>" + yellVal + " <button class='delbutton'>x</button></li>"  )
           //clear input field
           return $('.yellbox').val("");
 
 
         } else if (yellVal === "" && (event.which === 13 || event.type === 'click') )
             return $(advice).css({display: 'block'})
-        }
+    }
+
+
+    var deleteyell = function(){
+       $(this).parent().remove() 
+    }
 
     $('.yellbutton').on('click', yell )
     $(".yellbox").on('keydown', yell)
+    $('ul').on('click',".delbutton",deleteyell)
 
 // end of document ready function...
 })
